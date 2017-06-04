3c0c103deeea181e91c14f8024c96a07bf91da45
Rithvik Patibandla <rithvikp98@gmail.com>
2017-06-04 16:28:37 +0530

    Following Qt coding style

--- main.cpp
+++ main.cpp
@@ -7,52 +7,43 @@
 #include <cups/cups.h>
 #include <QMessageLogger>
 
-class pdf_preview : public QQuickImageProvider {
-
+class QPdfPreview : public QQuickImageProvider
+{
 public:
-    pdf_preview()
-        : QQuickImageProvider(QQuickImageProvider::Image)
-    {
-    }
+    QPdfPreview() : QQuickImageProvider(QQuickImageProvider::Image) {}
 
     QImage requestImage(const QString &id, QSize *size, const QSize &requestedSize)
     {
 
-        Poppler::Document* document = Poppler::Document::load("/tmp/test.pdf");
+        Poppler::Document *document = Poppler::Document::load("/tmp/test.pdf");
         if (!document || document->isLocked())
-        {
             qCritical("File '%s' does not exist or is locked!", qUtf8Printable("/tmp/test.pdf"));
-        }
 
-        Poppler::Page* pdfPage = document->page(0);
+        Poppler::Page *pdfPage = document->page(0);
         if (pdfPage == nullptr)
-        {
             qCritical("File '%s' is empty?", qUtf8Printable("/tmp/test.pdf"));
-        }
 
         QImage image = pdfPage->renderToImage(72.0,72.0,0,0,pdfPage->pageSize().width(),pdfPage->pageSize().height());
         if (image.isNull())
-        {
             qCritical("Error!");
-        }
 
         return image;
 
-        //delete document and image after use
+        //addImageProvider handles the destruction of the object
     }
 };
 
-QStringList printers_list;
+QStringList printersList;
 
-void set_printers_list(){
+void qSetPrintersList() {
     cups_dest_t *dests;
-    int num_dests = cupsGetDests(&dests);
-    cups_dest_t *dest = cupsGetDest("name", nullptr, num_dests, dests);
-    for(dest = dests; dest != & dests[num_dests]; dest++)
+    int numDests = cupsGetDests(&dests);
+    cups_dest_t *dest = cupsGetDest("name", nullptr, numDests, dests);
+    for(dest = dests; dest != & dests[numDests]; dest++)
         if (dest->instance == nullptr)
-            printers_list.append(dest->name);
+            printersList.append(dest->name);
 
-    cupsFreeDests(num_dests, dests);
+    cupsFreeDests(numDests, dests);
     return;
 }
 
@@ -60,11 +51,11 @@ int main(int argc, char *argv[]) {
 
     QGuiApplication app(argc, argv);
 
-    set_printers_list();
+    qSetPrintersList();
 
     QQuickView view;
-    view.rootContext()->setContextProperty("printersListModel", QVariant::fromValue(printers_list));
-    view.engine()->addImageProvider(QLatin1String("preview"), new pdf_preview);
+    view.rootContext()->setContextProperty("printersListModel", QVariant::fromValue(printersList));
+    view.engine()->addImageProvider(QLatin1String("preview"), new QPdfPreview);
     view.setSource(QUrl("qrc:/main.qml"));
     view.show();
 
